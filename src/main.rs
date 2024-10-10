10a1a1c44aeed815607125032dff1c26e8d64b88
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2024-10-10 10:47:57 +0200

    Add basic CLI

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::Arg;
+use clap::ArgAction;
+use clap::Command;
+
 fn main() {
-    println!("Hello, world!");
+    let matches = Command::new("rlux")
+        .version("1.0")
+        .author("Author Name <frankhampusweslien@gmail.com>")
+        .about("Does awesome things")
+        .subcommand(
+            Command::new("run").about("Runs the application").arg(
+                Arg::new("filepath")
+                    .help("The path to the file to run")
+                    .action(ArgAction::Set)
+                    .value_name("FILE")
+                    .required(true)
+                    .index(1),
+            ),
+        )
+        .subcommand(Command::new("repl").about("Starts a REPL"))
+        .get_matches();
+
+    match matches.subcommand() {
+        Some(("run", args)) => {
+            match args.get_one::<String>("filepath") {
+                Some(filepath) => {
+                    println!("Running with file: {}", filepath);
+                    // Add your file processing logic here
+                }
+                None => println!("No filepath was provided"),
+            }
+        }
+        Some(("repl", _)) => {
+            println!("Starting REPL...");
+            // Add your REPL logic here
+        }
+        _ => println!("No valid subcommand was used"),
+    }
 }
