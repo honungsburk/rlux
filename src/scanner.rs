724796acbd3500dc03ea69505bd0165056c4f6e4
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2024-10-10 13:00:16 +0200

    Add all TokenTypes

--- src/scanner.rs
+++ src/scanner.rs
@@ -1,27 +1,106 @@
-use std::fmt::Display;
+use std::fmt::{write, Display};
 
 #[derive(Debug, PartialEq, Clone)]
-pub enum Token {
+pub enum TokenType {
+    // Single-character tokens.
     LeftParen,
     RightParen,
+    LeftBrace,
+    RightBrace,
+    Comma,
+    Dot,
+    Minus,
+    Plus,
+    Semicolon,
+    Slash,
+    Star,
+    // One or two character tokens.
+    Bang,
+    BangEqual,
+    Equal,
+    EqualEqual,
+    Greater,
+    GreaterEqual,
+    Less,
+    LessEqual,
+    // Literals
+    Identifier(String),
+    String(String),
+    Number(f64),
+    // Keywords
+    And,
+    Class,
+    Else,
+    False,
+    Fun,
+    For,
+    If,
+    Nil,
+    Or,
+    Print,
+    Return,
+    Super,
+    This,
+    True,
+    Var,
+    While,
+    // End of file
+    Eof,
 }
 
-impl Display for Token {
+impl Display for TokenType {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
         match self {
-            Token::LeftParen => write!(f, "("),
-            Token::RightParen => write!(f, ")"),
+            TokenType::LeftParen => write!(f, "("),
+            TokenType::RightParen => write!(f, ")"),
+            TokenType::LeftBrace => write!(f, "{}", "{"),
+            TokenType::RightBrace => write!(f, "{}", "}"),
+            TokenType::Comma => write!(f, ","),
+            TokenType::Dot => write!(f, "."),
+            TokenType::Minus => write!(f, "-"),
+            TokenType::Plus => write!(f, "+"),
+            TokenType::Semicolon => write!(f, ";"),
+            TokenType::Slash => write!(f, "/"),
+            TokenType::Star => write!(f, "*"),
+            TokenType::Bang => write!(f, "!"),
+            TokenType::BangEqual => write!(f, "!="),
+            TokenType::Equal => write!(f, "="),
+            TokenType::EqualEqual => write!(f, "=="),
+            TokenType::Greater => write!(f, ">"),
+            TokenType::GreaterEqual => write!(f, ">="),
+            TokenType::Less => write!(f, "<"),
+            TokenType::LessEqual => write!(f, "<="),
+            TokenType::Identifier(s) => write!(f, "{}", s),
+            TokenType::String(s) => write!(f, "\"{}\"", s),
+            TokenType::Number(n) => write!(f, "{}", n),
+            TokenType::And => write!(f, "and"),
+            TokenType::Class => write!(f, "class"),
+            TokenType::Else => write!(f, "else"),
+            TokenType::False => write!(f, "false"),
+            TokenType::Fun => write!(f, "fun"),
+            TokenType::For => write!(f, "for"),
+            TokenType::If => write!(f, "if"),
+            TokenType::Nil => write!(f, "nil"),
+            TokenType::Or => write!(f, "or"),
+            TokenType::Print => write!(f, "print"),
+            TokenType::Return => write!(f, "return"),
+            TokenType::Super => write!(f, "super"),
+            TokenType::This => write!(f, "this"),
+            TokenType::True => write!(f, "true"),
+            TokenType::Var => write!(f, "var"),
+            TokenType::While => write!(f, "while"),
+            TokenType::Eof => write!(f, "EOF"),
         }
     }
 }
 
-pub fn run(source: &str) -> Vec<Token> {
+pub fn run(source: &str) -> Vec<TokenType> {
     let mut tokens = Vec::new();
 
     for c in source.chars() {
         match c {
-            '(' => tokens.push(Token::LeftParen),
-            ')' => tokens.push(Token::RightParen),
+            '(' => tokens.push(TokenType::LeftParen),
+            ')' => tokens.push(TokenType::RightParen),
             _ => {}
         }
     }
