95128c4e5de7d15170e09307680355aa6102e518
Frank Hampus Weslien <frankhampusweslien@gmail.com>
2024-10-29 14:45:10 +0100

    refactor: remove unused code

--- src/interpreter/lib.rs
+++ src/interpreter/lib.rs
@@ -2,42 +2,8 @@
 //!
 //!
 
-use super::{Environment, LuxCallable, LuxValue};
-use std::{
-    fmt,
-    time::{SystemTime, UNIX_EPOCH},
-};
-
-#[derive(Debug)]
-struct Clock;
-
-impl fmt::Display for Clock {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        write!(f, "<native clock>") // Customize as needed
-    }
-}
-
-impl LuxCallable for Clock {
-    fn arity(&self) -> usize {
-        0
-    }
-
-    fn call(
-        self: std::rc::Rc<Self>,
-        _interpreter: &mut super::Interpreter,
-        _args: &[super::LuxValue],
-    ) -> Result<super::LuxValue, super::RunTimeError> {
-        let start = SystemTime::now();
-        let since_the_epoch = start
-            .duration_since(UNIX_EPOCH)
-            .expect("Time went backwards");
-
-        let in_ms =
-            since_the_epoch.as_secs() * 1000 + since_the_epoch.subsec_nanos() as u64 / 1_000_000;
-
-        Ok(super::LuxValue::Number(in_ms as f64))
-    }
-}
+use super::{Environment, LuxValue};
+use std::time::{SystemTime, UNIX_EPOCH};
 
 /// Load the standard library into an environment.
 pub fn load(env: &mut Environment) {
